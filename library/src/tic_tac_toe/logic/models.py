e73be8dbd61ad6065235a116bc95adcc6c0afcbc
Nguyen King <kingnguyen0123@gmail.com>
2023-02-13 10:28:58 +0700

    Add property other in Mark

--- library/src/tic_tac_toe/logic/models.py
+++ library/src/tic_tac_toe/logic/models.py
@@ -3,3 +3,7 @@ import enum
 class Mark(enum.StrEnum):
     CROSS = "X"
     NAUGHT = "O"
+
+    @property
+    def other(self) -> "Mark":
+        return Mark.CROSS if self is Mark.NAUGHT else Mark.NAUGHT
