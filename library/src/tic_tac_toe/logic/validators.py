563389b99df01a6c1705c350ddace572e0343c06
Nguyen King <kingnguyen0123@gmail.com>
2023-02-15 15:53:20 +0700

    [validators] Validate the starting mark

--- library/src/tic_tac_toe/logic/validators.py
+++ library/src/tic_tac_toe/logic/validators.py
@@ -3,7 +3,7 @@ from  __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from tic_tac_toe.logic.models import GameState, Grid
+    from tic_tac_toe.logic.models import GameState, Grid, Mark
 
 import re
 
@@ -21,3 +21,11 @@ def validate_game_state(game_state: GameState) -> None:
 def validate_number_of_marks(grid: Grid) -> None:
     if abs(grid.x_count - grid.o_count) > 1:
         raise InvalidGameState("Wrong number of Xs and Os")
+
+def validate_starting_mark(grid: Grid, starting_mark: Mark) -> None:
+    if grid.x_count > grid.o_count:
+        if starting_mark != "X":
+            raise InvalidGameState("Wrong starting mark")
+    elif grid.o_count > grid.x_count:
+        if starting_mark != "O":
+            raise InvalidGameState("Wrong starting mark")
